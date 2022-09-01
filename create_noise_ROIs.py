24b87e244ac273857ccab811c65b94d6bc3848b5
Ondrej Burkot <burkot@adelie.fnol.loc>
2022-09-01 14:13:46 +0200

    4 background square ROIs, 2D and 3D mask

--- create_noise_ROIs.py
+++ create_noise_ROIs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 #
 # Create 4 cubic ROIs in the input image corners. The ROIs can be used for computation of background noise SD.
+# Input image should have same number of rows and columns, otherwise script will fail.
 #
 # Example:
-#       python3 create_noise_ROIs.py -i <input_image> -shiftx 10 -shifty 10 -shiftunits px -size 15
+#       python3 create_noise_ROIs.py -i <input_image> -shiftx 10 -shifty 10 -shiftunits pix -size 15
 #
 # Authors: Ondrej Burkot, Jan Valosek
 #
@@ -30,11 +31,12 @@ def get_parser():
     parser.add_argument('-shiftunits', help='Shift units in pixels (pix) or percentage (per)', type=str,
                         required=False, default='pix', choices=['pix', 'per'])
     parser.add_argument('-size', help='Size of the ROIs in pixels. Example: 10', type=int, required=False, default=10)
+    parser.add_argument('-visualise', help='Visualisation of created ROI for debug. 0 - do not visualise, 1 - visualise',
+                        type=int, required=False, default=0)
     return parser
 
 
 def main():
-
     # Parse the command line arguments
     parser = get_parser()
     args = parser.parse_args()
@@ -53,14 +55,20 @@ def main():
     shift_x = args.shiftx
     shift_y = args.shifty
     shift_units = args.shiftunits
+    visualise = args.visualise
 
-    # testImg1 = testImg1.get_fdata()[:, :, 2]
-    positionShift = 5                                   # value in %, should cover noise in background
-    slice = 2
-    color = 'Blues'
+    test_img1_hdr = testImg1.header
+    test_img1_affine = testImg1.affine
+    slice = 3
 
-    testImg1Hdr = testImg1.header
-    nx, ny, nz = testImg1Hdr.get_data_shape()           # data shape, number of pixels in x, y, z
+    # Converting to 2D for testing 2D ROI selection
+    # testImg1 = testImg1.get_fdata()[:, :, slice]
+
+    color = 'Blues'                              # setting color for visualisation
+
+    nx, ny, nz = test_img1_hdr.get_data_shape()  # data shape, number of pixels in x, y, z
+    # convert NiftiImage class to ndarray
+    testImg1 = testImg1.get_fdata()
 
     if shift_units == 'pix':
         # center of ROI [x, y, z]
@@ -69,56 +77,92 @@ def main():
                   nz]
     elif shift_units == 'per':
         # center of ROI [x, y, z]
-        center = [np.ceil(nx*shift_x/100),
-                  np.ceil(ny*shift_y/100),
+        center = [np.ceil(nx * shift_x / 100),
+                  np.ceil(ny * shift_y / 100),
                   nz]
 
     # mask for 3D volume
-    if nz > 1:
+    if testImg1.ndim > 2:
 
-        # initialize 2d grid
+        # initialize 3d grid
         xx, yy, zz = np.mgrid[:nx, :ny, :nz]
+        mask3d_final = np.zeros((nx, ny, nz))
         xc = center[0]
         yc = center[1]
         zc = center[2]
 
-        radius_x = np.ceil((int(size) - 1) / 2)         # radius of square - 5 to left, 5 to right
-        radius_y = radius_x                             # square --> radius left-right and top-bottom are same
+        radius_x = np.ceil((int(size) - 1) / 2)  # radius of square - 5 to left, 5 to right
+        radius_y = radius_x  # square --> radius left-right and top-bottom are same
         radius_z = nz
 
-        # * 1 to convert F or T to 0 or 1
-        mask3d = ((abs(xx - xc) <= radius_x) &
-                  (abs(yy - yc) <= radius_y) &
-                  (abs(zz - zc) <= radius_z)) * 1
+        for it in range(4):
+            mask3d = ((abs(xx - xc) <= radius_x) &
+                      (abs(yy - yc) <= radius_y) &
+                      (abs(zz - zc) <= radius_z)) * 1   # * 1 to convert F or T to 0 or 1
+            mask3d = np.rot90(mask3d, k=it)
+            mask3d_final = mask3d_final + mask3d
+            testImg1 = np.rot90(testImg1, k=it)
 
-        plt.figure()
-        plt.imshow(testImg1.get_fdata()[:, :, slice].T, cmap='gray')
-        plt.imshow(np.transpose(mask3d[:, :, slice]), cmap='Blues', alpha=0.3)
-        plt.show()
+        # pix_intensity = np.where(mask2d_final == 1, testImg1, False)
+        # std = np.std(pix_intensity)
+
+        # save selected ROI to NIfTI
+        mask3d_final = nib.Nifti2Image(mask3d_final, affine=test_img1_affine, header=test_img1_hdr, dtype='uint8')
+        nib.save(mask3d_final, 'noise_mask_3d_hdr.nii')
+
+        # Possible visualisation but for some reason takes a lot of time
+        if visualise == 1:
+            ax = plt.figure().add_subplot(projection='3d')
+            ax.voxels(mask3d_final, facecolors='red')
+            plt.show()
+
+            # Visualisation in 2D plane
+            # plt.figure()
+            # plt.imshow(testImg1[:, :, slice].T, cmap='gray')
+            # plt.imshow(mask3d_final[:, :, slice].T, cmap=color, alpha=0.3)
+            # plt.show()
 
     # mask for single 2D slice
     else:
 
         # initialize 2d grid
         xx, yy = np.mgrid[:nx, :ny]
+        mask2d_final = np.zeros((nx, ny))
+        pix_intensity = np.zeros((nx, ny))
         xc = center[0]
         yc = center[1]
 
-        radius_x = np.ceil((int(size) - 1) / 2)         # radius of rectangular - 5 to left, 5 to right
-        radius_y = radius_x                             # rectangular --> radius left-right and top-bottom are same
+        radius_x = np.ceil((int(size) - 1) / 2)         # radius of square - 5 to left, 5 to right
+        radius_y = radius_x                             # square --> radius left-right and top-bottom are same
 
-        # * 1 to convert F or T to 0 or 1
-        mask2d = ((abs(xx - xc) <= radius_x) &
-                  (abs(yy - yc) <= radius_y)) * 1
+        for it in range(4):
+            mask2d = ((abs(xx - xc) <= radius_x) &
+                      (abs(yy - yc) <= radius_y)) * 1   # * 1 to convert F or T to 0 or 1
+            mask2d = np.rot90(mask2d, k=it)
+            mask2d_final = mask2d_final + mask2d
+            testImg1 = np.rot90(testImg1, k=it)
 
-        plt.figure()
-        plt.imshow(testImg1.get_fdata()[:, :, slice].T, cmap='gray')
-        plt.imshow(np.transpose(mask2d[:, :]), cmap=color, alpha=0.3)
-        plt.show()
+        # Visualisation in 2D plane
+        if visualise == 1:
+            plt.figure()
+            plt.imshow(np.rot90(testImg1, k=1), cmap='gray')
+            plt.imshow(mask2d_final, cmap=color, alpha=0.3)
+            plt.show()
 
-    # testImg2 = nib.load('sub-5147B_T2star.nii.gz')
-    # testImg1Affine = testImg1.affine.copy()
-    # testImg1Header = testImg1.header.copy()
+        # getting pixel intensities from mask
+        pix_intensity = np.where(mask2d_final == 1, testImg1, False)
+        std = np.std(pix_intensity)
+
+        # save mask to NIfTI
+        # I think it makes no sense to save 2D mask to a nifti file, but may be useful...
+        # mask2d_final = nib.Nifti2Image(mask2d_final, affine=test_img1_affine)
+        # nib.save(mask2d_final, 'noise_mask_2d.nii')
+
+        # appending values of mask2d to mask2d_final if
+        # count of rows and columns of input image are not equal!!!
+
+        txt = 'Standard deviation value is: {0:.5f}'
+        print(txt.format(std))
 
 
 if __name__ == "__main__":
